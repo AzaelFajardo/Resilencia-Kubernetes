b3cf90c05fa8b36ab094d031ed09163629d96c83
Gael1435 <elirivera_2782@hotmail.com>
2026-03-19 16:50:03 -0600

    mini servicio order-service

--- services/notification-service/main.py
+++
@@ -1,8 +0,0 @@
-#Logica de el servicio de ordenes
-from fastapi import FastAPI
-
-app = FastAPI()
-
-@app.get("/notify")
-def send_notification():
-    return {"status": "sent", "message": "Notification delivered"}
